57d285ca09964e137f288d18a4b61976fecce6ba
nstarlike <46280877+nstarlike@users.noreply.github.com>
2023-05-25 13:13:05 +0900

    Add Spring Boot project

---
+++ src/main/java/nstarlike/ls/LogisticsSystemApplication.java
@@ -0,0 +1,13 @@
+package nstarlike.ls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticsSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticsSystemApplication.class, args);
+	}
+
+}
